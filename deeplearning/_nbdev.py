8cc2842e5fb4e9b0b09513f708f75ff5bc3dc27e
Konstantin Dorichev <kdorichev@gmail.com>
2019-12-04 10:06:19 +0300

    plotdist() added to core

--- deeplearning/_nbdev.py
+++ deeplearning/_nbdev.py
@@ -3,7 +3,8 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"stats": "00_core.ipynb",
-         "log_softmax": "00_core.ipynb"}
+         "log_softmax": "00_core.ipynb",
+         "plotdist": "00_core.ipynb"}
 
 modules = ["core.py"]
 
